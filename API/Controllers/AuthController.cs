f99872ee06a42f0c9afa6d3fa352a4e553a1c26e
albionramadani <albionr9@gmail.com>
2021-08-27 01:07:24 +0200

    login and authenticated controller was added successfully

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        //Post /api/auth
+        [HttpPost]
+        public ActionResult Login([FromBody] Login login)
+        {
+            if(login.Username =="admin" && login.Password == "pass")
+            {
+                return Json(new { succses = true });
+            }
+            else
+            {
+                return Json(new { succses = false });
+            }
+        }
+    }
+}
